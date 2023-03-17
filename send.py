6197e57ca658ef8eea812b01369a9be8c8883904
dk8827 <121302433+dk8827@users.noreply.github.com>
2023-03-17 11:29:48 +0200

    fix

--- send.py
+++ send.py
@@ -36,6 +36,6 @@ while (len(phoneNumbersToSendTo)>0):
     randomPhone=random.choice(phoneNumbersToSendTo)
     phoneNumbersToSendTo.remove(randomPhone)
     randomMessage=random.choice(MESSAGES)
-    pywhatkit.sendwhatmsg_instantly(phone_no= randomPhone, message= "hello", wait_time= 10, tab_close= True, close_time= 3)
+    pywhatkit.sendwhatmsg_instantly(phone_no= randomPhone, message= randomMessage, wait_time= 10, tab_close= True, close_time= 3)
     print(f"Sending message {randomMessage} to {randomPhone}")
     time.sleep(TIME_TO_WAIT_BETWEEN_MESSAGES_SECONDS)
